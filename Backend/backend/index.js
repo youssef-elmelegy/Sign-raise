250686fc6d0cffeb33079257b187d435e7577b32
youssef-elmelegy <youssefelmelegy999@gmail.com>
2025-03-15 00:17:33 +0200

    feat: handle cors logic

--- Backend/backend/index.js
+++ Backend/backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"; // Import cors
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
@@ -8,6 +9,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+app.use(
+  cors({
+    origin: CLIENT_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
