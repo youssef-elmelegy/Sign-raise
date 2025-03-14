352f811885673f396724c4405f35b8f233c79999
DINAMOW <meemoo102039@gmail.com>
2025-03-15 01:45:45 +0200

    refactor: simplify CORS setup and improve Swagger API documentation path resolution

--- Backend/backend/routes/auth.route.js
+++ Backend/backend/routes/auth.route.js
@@ -14,6 +14,52 @@ import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 format: string
+ *                 example: John Doe
+ *               password:
+ *                 type: string
+ *                 format: password
+ *               email:
+ *                 type: string
+ *                 format: email
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             example:
+ *               success: true
+ *               message: User created successfully
+ *               user:
+ *                 id: "0a8d4830-81a2-4a4b-bde6-b0890df91ca1"
+ *                 email: "user@example.com"
+ *                 name: "John Doe"
+ *                 lastLogin: "2025-03-14T23:28:20.911Z"
+ *                 isVerified: false
+ *                 resetPasswordToken: null
+ *                 resetPasswordExpiresAt: null
+ *                 verificationToken: "748330"
+ *                 verificationTokenExpiresAt: "2025-03-14T23:38:20.910Z"
+ *                 createdAt: "2025-03-14T23:28:20.911Z"
+ *                 updatedAt: "2025-03-14T23:28:20.911Z"
+ *       400:
+ *         description: Bad request
+ */
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
