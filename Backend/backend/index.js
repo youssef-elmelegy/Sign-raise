5176bb9ae8747d7573cb2e6a980cab65ea2f98bf
youssef-elmelegy <youssefelmelegy999@gmail.com>
2025-03-16 22:28:33 +0200

    fix: cors error

--- Backend/backend/index.js
+++ Backend/backend/index.js
@@ -13,33 +13,43 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-// const CLIENT_URL = process.env.CLIENT_URL;
+const allowedOrigins = [process.env.FRONTEND_URL, process.env.FRONTEND_URL2];
 
-app.use(cors(
-    {
-        origin: '*',
-        credentials: true,
-    }
-));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
-connectDB().catch(
-    (error) => console.error("Error connecting to the database: ", error.message)
-)
+connectDB().catch((error) =>
+  console.error("Error connecting to the database: ", error.message)
+);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 app.get("/", (req, res) => {
-res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/daily", dailyRoutes);
 
 app.listen(PORT, "0.0.0.0", () => {
-console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
 
 const nms = new NodeMediaServer(config);
