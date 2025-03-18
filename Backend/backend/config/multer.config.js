4d2beb00cc714e485d77e089ec74c6808e3ce2d1
JO.O PRo <115453231+youssef-elmelegy@users.noreply.github.com>
2025-03-18 21:38:14 +0200

    Update multer.config.js

--- Backend/backend/config/multer.config.js
+++ Backend/backend/config/multer.config.js
@@ -1,13 +1,19 @@
 import multer from "multer";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const uploadDir = path.join(__dirname, "../../uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadDir = "uploads/";
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
-    }
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
@@ -18,7 +24,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage,
   fileFilter: (req, file, cb) => {
-    console.log("Uploaded file MIME type:", file.mimetype); // Debugging
+    console.log("Uploaded file MIME type:", file.mimetype);
     const allowedTypes = ["audio/mpeg", "audio/wav", "audio/webm", "audio/mp3"];
     if (allowedTypes.includes(file.mimetype)) {
       cb(null, true);
